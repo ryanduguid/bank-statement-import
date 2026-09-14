6a09003c7fb07bf287071be48dfd39674c239eed
Ryan Duguid <ryanduguid@users.noreply.github.com>
2026-09-15 09:42:21 +1000

    test: use valid bank account matching fixtures

--- account_statement_import_file/tests/test_account_statement_import_file.py
+++ account_statement_import_file/tests/test_account_statement_import_file.py
@@ -54,14 +54,18 @@ class TestAccountStatementImportFile(common.TransactionCase):
             import_wizard._complete_stmts_vals(stmts_vals, self.journal_1, "1111111111")
 
     def test_exact_account_precedes_substring(self):
-        self.bank_account.acc_number = "9333333333"
+        self.journal_1.bank_account_id = self.env["res.partner.bank"].create(
+            {"acc_number": "93333333333", "partner_id": self.env.company.partner_id.id}
+        )
         self.journal_2.bank_account_id = self.bank_account_2
         journal = self.import_wizard._find_bank_journal("3333333333")
         self.assertEqual(journal, self.journal_2)
 
     def test_partial_account_must_be_unambiguous(self):
-        self.bank_account.acc_number = "9333333333"
-        self.bank_account_2.acc_number = "8333333333"
+        self.journal_1.bank_account_id = self.env["res.partner.bank"].create(
+            {"acc_number": "93333333333", "partner_id": self.env.company.partner_id.id}
+        )
+        self.bank_account_2.acc_number = "83333333333"
         self.journal_2.bank_account_id = self.bank_account_2
         with self.assertRaises(UserError):
             self.import_wizard._find_bank_journal("3333333333")
